f4f634bfca45002fd3b6b605c518f0fa5f538431
Alexander Q <qalexx@gmail.com>
2023-05-22 11:59:06 +0300

    colorschemes and stuff

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,25 @@
 return {
     {
-        "marko-cerovac/material.nvim",
+        'sainnhe/sonokai',
         lazy = false,
-        priority = 1000,
         config = function()
-            vim.cmd'colorscheme material'
+            vim.g.sonokai_disable_italic_comment = 1
+            vim.g.sonokai_style = 'default'
+            vim.cmd'colorscheme sonokai'
+        end
+    },
+    {
+        "marko-cerovac/material.nvim",
+        lazy = true,
+        config = function()
             require'material.functions'.change_style("darker")
+
+            ColorschemeMaterialWithStyle = function()
+                vim.cmd'colorscheme material'
+                require'material.functions'.find_style()
+            end
+
+            vim.api.nvim_create_user_command('MaterialWithStyle', ColorschemeMaterialWithStyle, {})
         end
     },
     {
@@ -28,5 +42,14 @@ return {
         'ViViDboarder/wombat.nvim',
         lazy = true,
         dependencies = {'rktjmp/lush.nvim'},
-    }
+    },
+    {
+        'sainnhe/everforest',
+        lazy = true
+    },
+    {
+        'sainnhe/edge',
+        lazy = true,
+    },
+
 }
