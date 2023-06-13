a90e94b5560c0aa1841039bc7061e3c1b0f66d5b
Alexander Q <qalexx@gmail.com>
2023-06-13 16:40:49 +0300

    nvim-tree: change root to current file

--- lua/plugins/90-nvim-tree.lua
+++ lua/plugins/90-nvim-tree.lua
@@ -2,10 +2,17 @@ return {
     {
         "nvim-tree/nvim-tree.lua",
         dependencies = {"nvim-tree/nvim-web-devicons"},
-        config = function()
+        opts = {
+            sync_root_with_cwd = true,
+            update_focused_file = {
+                enable = true,
+                update_root = true,
+            }
+        },
+        config = function(_, opts)
             vim.g.loaded_netrw = 1
             vim.g.loaded_netrwPlugin = 1
-            require'nvim-tree'.setup()
+            require'nvim-tree'.setup(opts)
 
             vim.keymap.set('n', '<C-f>', '<cmd>NvimTreeToggle<cr>', {desc = "NvimTree toggle"})
         end,
