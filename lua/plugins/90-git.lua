816834548ad98b425f594edb2750689957ff9fee
Alexander Q <qalexx@gmail.com>
2023-07-17 10:30:20 +0300

    stuff

--- lua/plugins/90-git.lua
+++ lua/plugins/90-git.lua
@@ -22,6 +22,8 @@ return {
         dependencies = {'nvim-lua/plenary.nvim'},
         config = function()
             vim.g.lazygit_floating_window_use_plenary = 1
+            require("telescope").load_extension("lazygit")
+            vim.keymap.set('n', '<leader>gg', '<cmd>LazyGit<cr>', {desc = "LazyGit"})
         end,
     }
 }
