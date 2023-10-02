b58121f95ad9b9f0bbc9c6de7a0689685387355b
Alexander Q <qalexx@gmail.com>
2023-10-02 12:23:13 +0300

    toggleterm

---
+++ lua/plugins/90-terminal.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        'akinsho/toggleterm.nvim',
+        opts = {
+            open_mapping = [[<c-`>]],
+            insert_mappings = true,
+            start_in_insert = true,
+            autochdir = true,
+        },
+    }
+}
