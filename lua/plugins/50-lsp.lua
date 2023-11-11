8c838aed8342d02f7c8e1f5ad5d38473ed1bcd7e
Alexander Q <qalexx@gmail.com>
2023-11-11 11:43:49 +0300

    pre-lspzero

--- lua/plugins/50-lsp.lua
+++ lua/plugins/50-lsp.lua
@@ -153,6 +153,9 @@ return {
             local liblldb_path = codelldb_root .. "lldb/lib/liblldb.so"
 
             opts = {
+                hover_actions = {
+                    auto_focus = true,
+                },
                 server = {
                     settings = {
                         ["rust-analyzer"] = {
@@ -167,8 +170,13 @@ return {
                     on_attach = function(_, bufnr)
                         print("rust LS attached")
                         vim.keymap.set('n', '<leader>R', rt.workspace_refresh.reload_workspace, {desc = 'Reload workspace', buffer = bufnr})
+                        vim.keymap.set("n", "<leader>A", rt.hover_actions.hover_actions, {desc = 'Rust hover action', buffer = bufnr})
+                        vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, {desc = 'Rust code action', buffer = bufnr})
+                        vim.keymap.set("n", "<Leader>D", rt.debuggables.debuggables, {desc = 'Rust debuggables', buffer = bufnr})
+                        vim.keymap.set("n", "<Leader>R", rt.runnables.runnables, {desc = 'Rust runnables', buffer = bufnr})
                     end,
                 },
+                dap = { adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path) },
             }
             rt.setup(opts)
 
