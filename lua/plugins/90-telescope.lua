1aab636da3b8b4a3f107e955a0ef736a8e0c5220
Alexander Q <qalexx@gmail.com>
2023-11-11 17:36:16 +0300

    stuff

--- lua/plugins/90-telescope.lua
+++ lua/plugins/90-telescope.lua
@@ -74,8 +74,8 @@ return {
                 return vim.fs.dirname(dir)
             end
 
-            vim.keymap.set('n', '<leader>f', builtin.git_files, {desc = 'Jump to file tracked by git'})
-            vim.keymap.set('n', '<leader> f', function() builtin.find_files{cwd = project_dir()} end, {desc = 'Jump to file'})
+            vim.keymap.set('n', '<leader> f', builtin.git_files, {desc = 'Jump to file tracked by git'})
+            vim.keymap.set('n', '<leader>f', function() builtin.find_files{cwd = project_dir()} end, {desc = 'Jump to file'})
             vim.keymap.set('n', '<leader>C', builtin.git_commits, {desc = 'Jump to git commit'})
             vim.keymap.set('n', '<leader>r', function() builtin.live_grep{cwd = project_dir()} end, {desc = 'Live grep with rg'})
 
