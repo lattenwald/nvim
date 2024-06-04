bc5aa92363e24fa46407f7359c0d1c5584a0f5ca
Alexander Q <qalexx@gmail.com>
2024-06-04 14:33:57 +0300

    keybindings

--- lua/plugins/90-git.lua
+++ lua/plugins/90-git.lua
@@ -30,6 +30,7 @@ return {
             vim.g.lazygit_floating_window_use_plenary = 1
             require("telescope").load_extension("lazygit")
             vim.keymap.set('n', '<leader>gg', '<cmd>LazyGit<cr>', {desc = "LazyGit"})
+            vim.keymap.set('n', '<leader>gG', function() require("telescope").extensions.lazygit.lazygit() end, {desc = "LazyGit (Telescope)"})
         end,
     }
 }
