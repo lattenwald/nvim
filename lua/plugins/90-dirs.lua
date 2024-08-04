f0adf9db9d8265b758049271995fcbc9041c3e33
Alexander Q <qalexx@gmail.com>
2024-08-04 18:17:37 +0300

    dirs stuff

--- lua/plugins/90-dirs.lua
+++ lua/plugins/90-dirs.lua
@@ -22,7 +22,7 @@ return {
             nvim_tree.disable_netrw = false
             nvim_tree.hijack_netrw  = true
 
-            vim.keymap.set('n', '<C-f>', '<cmd>NvimTreeToggle<cr>', {desc = "NvimTree toggle"})
+            vim.keymap.set('n', '<C-S-f>', '<cmd>NvimTreeToggle<cr>', {desc = "NvimTree toggle"})
         end,
     },
     {
@@ -41,11 +41,12 @@ return {
             follow_current_file = {
                 enabled = true,
             },
+            use_libuv_file_watcher = true,
         },
         config = function(_, opts)
             require'neo-tree'.setup(opts)
 
-            vim.keymap.set('n', '<C-S-f>', '<cmd>Neotree reveal toggle<cr>', {desc = "Neotree current file"})
+            vim.keymap.set('n', '<C-f>', '<cmd>Neotree reveal toggle<cr>', {desc = "Neotree current file"})
             vim.keymap.set('n', '<C-b>', '<cmd>Neotree buffers float toggle<cr>', {desc = "Neotree buffers"})
         end,
     },
