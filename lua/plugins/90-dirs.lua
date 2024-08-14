5f226d484d19be4641f6df549c0970328e75e348
Alexander Q <qalexx@gmail.com>
2024-08-14 14:48:51 +0300

    dirs

--- lua/plugins/90-dirs.lua
+++ lua/plugins/90-dirs.lua
@@ -1,5 +1,51 @@
 return {
     {
+        "mikavilpas/yazi.nvim",
+        event = "VeryLazy",
+        keys = {
+            -- 👇 in this section, choose your own keymappings!
+            {
+                "<leader>-",
+                "<cmd>Yazi<cr>",
+                desc = "Open yazi at the current file",
+            },
+            {
+                -- Open in the current working directory
+                "<leader>cw",
+                "<cmd>Yazi cwd<cr>",
+                desc = "Open the file manager in nvim's working directory" ,
+            },
+            {
+                -- NOTE: this requires a version of yazi that includes
+                -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+                '<c-up>',
+                "<cmd>Yazi toggle<cr>",
+                desc = "Resume the last yazi session",
+            },
+        },
+        ---@type YaziConfig
+        opts = {
+            -- if you want to open yazi instead of netrw, see below for more info
+            open_for_directories = false,
+
+            -- enable these if you are using the latest version of yazi
+            -- use_ya_for_events_reading = true,
+            -- use_yazi_client_id_flag = true,
+
+            keymaps = {
+                show_help = '<f1>',
+            },
+        },
+    },
+    {
+        'stevearc/oil.nvim',
+        enabled = false,
+        opts = {},
+        -- Optional dependencies
+        -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+        dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    },
+    {
         "nvim-tree/nvim-tree.lua",
         dependencies = {"nvim-tree/nvim-web-devicons"},
         opts = {
@@ -22,11 +68,12 @@ return {
             nvim_tree.disable_netrw = false
             nvim_tree.hijack_netrw  = true
 
-            vim.keymap.set('n', '<C-S-f>', '<cmd>NvimTreeToggle<cr>', {desc = "NvimTree toggle"})
+            vim.keymap.set('n', '<C-f>', '<cmd>NvimTreeToggle<cr>', {desc = "NvimTree toggle"})
         end,
     },
     {
         'nvim-neo-tree/neo-tree.nvim',
+        enabled = false,
         dependencies = {
             'nvim-lua/plenary.nvim',
             'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
