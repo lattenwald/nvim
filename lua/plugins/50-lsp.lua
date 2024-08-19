061d1573b1aa7e4500a8c147e87119f1bff528d8
Alexander Q <qalexx@gmail.com>
2024-08-19 14:09:42 +0300

    gopls setup, updates

--- lua/plugins/50-lsp.lua
+++ lua/plugins/50-lsp.lua
@@ -60,8 +60,20 @@ return {
             lspconfig.ansiblels.setup{
                 capabilities = cmp_capabilities
             }
+
             lspconfig.gopls.setup{
-                capabilities = cmp_capabilities
+                capabilities = cmp_capabilities,
+                -- root_dir = function(fname)
+                --     -- see: https://github.com/neovim/nvim-lspconfig/issues/804
+                --     local mod_cache = vim.trim(vim.fn.system 'go env GOMODCACHE')
+                --     if fname:sub(1, #mod_cache) == mod_cache then
+                --         local clients = vim.lsp.get_active_clients { name = 'gopls' }
+                --         if #clients > 0 then
+                --             return clients[#clients].config.root_dir
+                --         end
+                --     end
+                --     return require'lspconfig.util'.root_pattern 'go.work'(fname) or util.root_pattern('go.mod', '.git')(fname)
+                -- end,
             }
 
             lspconfig.phpactor.setup{
