09ad171e51d40ce441af0ae7b16a238c4985297d
Alexander Q <qalexx@gmail.com>
2024-10-29 11:52:12 +0300

    stuff

--- lua/plugins/90-r.lua
+++ lua/plugins/90-r.lua
@@ -4,9 +4,15 @@ return {
         -- to start: \rf in .R file
         'R-nvim/R.nvim',
         lazy = false,
+        enabled = function()
+            vim.fn.executable("r")
+        end,
     },
     {
         'R-nvim/cmp-r',
         opts = {},
+        enabled = function()
+            vim.fn.executable("r")
+        end,
     }
 }
