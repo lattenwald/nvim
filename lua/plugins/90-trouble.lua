12ec8d1584e31d2b21d9765ecc0617dd5256e6df
Alexander Q <qalexx@gmail.com>
2025-04-18 14:54:48 +0300

    feat: <leader>q and <leader>V now open Trouble windows in current tab, closing in others

--- lua/plugins/90-trouble.lua
+++ lua/plugins/90-trouble.lua
@@ -3,17 +3,20 @@ return {
         "folke/trouble.nvim",
         opts = {},
         cmd = "Trouble",
-        keys = {
-            {
-                "<leader>q",
-                "<cmd>Trouble diagnostics<cr>",
-                desc = "Diagnostics (Trouble)",
-            },
-            {
-                "<leader>V",
-                "<cmd>Trouble symbols toggle<cr>",
-                desc = "Symbols (Trouble)",
-            },
-        },
+        config = function()
+            local trouble = require("trouble")
+            vim.keymap.set({ "n", "v" }, "<leader>q", function()
+                if trouble.is_open("diagnostics") then
+                    trouble.close("diagnostics")
+                end
+                trouble.open("diagnostics")
+            end, { desc = "Diagnostics (Trouble)" })
+            vim.keymap.set({ "n", "v" }, "<leader>V", function()
+                if trouble.is_open("symbols") then
+                    trouble.close("symbols")
+                end
+                trouble.open("symbols")
+            end, { desc = "Symbols (Trouble)" })
+        end,
     },
 }
