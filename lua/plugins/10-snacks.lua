13a5e274f97360051adc7cebcc68fa819b82ee04
Alexander Q <qalexx@gmail.com>
2025-05-06 13:01:22 +0300

    feat: go to def/impl/ref/typedef/decl now show preview in Snacks.picker() even if only one destination is available

--- lua/plugins/10-snacks.lua
+++ lua/plugins/10-snacks.lua
@@ -52,13 +52,13 @@ return {
             { "<leader>f", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
 
             -- LSP
-            { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
-            { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
-            { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
-            { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
-            { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+            { "gd", function() Snacks.picker.lsp_definitions({auto_confirm = false}) end, desc = "Goto Definition" },
+            { "gD", function() Snacks.picker.lsp_declarations({auto_confirm = false}) end, desc = "Goto Declaration" },
+            { "gr", function() Snacks.picker.lsp_references({auto_confirm = false, include_current = true}) end, nowait = true, desc = "References" },
+            { "gI", function() Snacks.picker.lsp_implementations({auto_confirm = false}) end, desc = "Goto Implementation" },
+            { "gy", function() Snacks.picker.lsp_type_definitions({auto_confirm = false}) end, desc = "Goto T[y]pe Definition" },
             { "<leader>i", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
-            { "<leader>I", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+            { "<leader>I", function() Snacks.picker.lsp_workspace_symbols({tree = true}) end, desc = "LSP Workspace Symbols" },
 
             -- git
             { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
