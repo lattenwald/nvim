564d543f728557a303b0690820a9cc8266c7a1e3
Alexander Q <qalexx@gmail.com>
2025-05-08 19:11:34 +0300

    fix: snacks priority

--- lua/plugins/10-snacks.lua
+++ lua/plugins/10-snacks.lua
@@ -1,6 +1,8 @@
 return {
     {
         "folke/snacks.nvim",
+        lazy = false,
+        priority = 1000,
         ---@type snacks.Config
         opts = {
             bigfile = { enabled = true },
