d45f22e46482586be2f4a2e122e92535250b7cad
Alexander Q <qalexx@gmail.com>
2025-05-14 11:11:30 +0300

    feat: most of debug keys are active only when debug session is active. <C-F5>, <F5> and <F8> are always active

--- lua/plugins/60-debug.lua
+++ lua/plugins/60-debug.lua
@@ -15,31 +15,59 @@ return {
             "mason-org/mason.nvim",
             "jay-babu/mason-nvim-dap.nvim",
         },
-        -- stylua: ignore
         config = function()
-            vim.keymap.set('n', '<C-F5>', function() require('dapui').toggle() end, {desc = "Debug: Show UI"})
-            vim.keymap.set('n', '<F5>', function() require('dap').continue() end, {desc = "Debug: Continue"})
-            vim.keymap.set('n', '<F8>', function() require('dap').toggle_breakpoint() end, {desc = "Debug: Toggle breakpoint"})
-            vim.keymap.set('n', '<F10>', function() require('dap').step_over() end, {desc = "Debug: Step over"})
-            vim.keymap.set('n', '<F11>', function() require('dap').step_into() end, {desc = "Debug: Step into"})
-            vim.keymap.set('n', '<F12>', function() require('dap').step_out() end, {desc = "Debug: Step out"})
-            vim.keymap.set('n', '<Leader>lb', function() require('dap').toggle_breakpoint() end, {desc = "Debug: Toggle breakpoint"})
-            vim.keymap.set('n', '<Leader>lB', function() require('dap').set_breakpoint() end, {desc = "debug: Set breakpoint"})
-            vim.keymap.set('n', '<Leader>lp', function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end, {desc = "Debug: Set log point"})
-            vim.keymap.set('n', '<Leader>lr', function() require('dap').repl.open() end, {desc = "Debug: Open REPL"})
-            vim.keymap.set('n', '<Leader>ll', function() require('dap').run_last() end, {desc = "Debug: Run last"})
+            local dap = require("dap")
+            local widgets = require("dap.ui.widgets")
 
-            vim.keymap.set({'n', 'v'}, '<Leader>lh', function() require('dap.ui.widgets').hover() end, {desc = "Debug: Hover"})
-            vim.keymap.set({'n', 'v'}, '<Leader>lp', function() require('dap.ui.widgets').preview() end, {desc = "Debug: Preview"})
-            vim.keymap.set('n', '<Leader>lf', function()
-                local widgets = require('dap.ui.widgets')
-                widgets.centered_float(widgets.frames)
-            end, {desc = "Debug: Frames"})
-            vim.keymap.set('n', '<Leader>ls', function()
-                local widgets = require('dap.ui.widgets')
-                widgets.centered_float(widgets.scopes)
-            end, {desc = "Debug: Scopes"})
+            -- Common keymaps
+            vim.keymap.set("n", "<C-F5>", function()
+                require("dapui").toggle()
+            end, { desc = "Debug: Show UI" })
+            vim.keymap.set("n", "<F5>", function()
+                dap.continue()
+            end, { desc = "Debug: Continue" })
+            vim.keymap.set("n", "<F8>", function()
+                dap.toggle_breakpoint()
+            end, { desc = "Debug: Toggle breakpoint" })
 
+            -- Keymaps only when DAP is active
+            -- stylua: ignore
+            local keymaps = {
+                -- {"<C-F5>", "n", function() require("dapui").toggle() end, {desc = "Debug: Show UI"}},
+                -- {"<F5>", "n", function() dap.continue() end, {desc = "Debug: Continue"}},
+                -- {"<F8>", "n", function() dap.toggle_breakpoint() end, {desc = "Debug: Toggle breakpoint"}},
+                {"<F10>", "n", function() dap.step_over() end, {desc = "Debug: Step over"}},
+                {"<F11>", "n", function() dap.step_into() end, {desc = "Debug: Step into"}},
+                {"<F12>", "n", function() dap.step_out() end, {desc = "Debug: Step out"}},
+                {"<Leader>lb", "n", function() dap.toggle_breakpoint() end, {desc = "Debug: Toggle breakpoint"}},
+                {"<Leader>lB", "n", function() dap.set_breakpoint() end, {desc = "debug: Set breakpoint"}},
+                {"<Leader>lp", "n", function() dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: ")) end, {desc = "Debug: Set log point"}},
+                {"<Leader>lr", "n", function() dap.repl.open() end, {desc = "Debug: Open REPL"}},
+                {"<Leader>ll", "n", function() dap.run_last() end, {desc = "Debug: Run last"}},
+
+                {"<Leader>lh", {"n", "v"}, function() widgets.hover() end, {desc = "Debug: Hover"}},
+                {"<Leader>lp", {"n", "v"}, function() widgets.preview() end, {desc = "Debug: Preview"}},
+                {"<Leader>lf", "n", function() widgets.centered_float(widgets.frames) end, {desc = "Debug: Frames"}},
+                {"<Leader>ls", "n", function() widgets.centered_float(widgets.scopes) end, {desc = "Debug: Scopes"}},
+            }
+
+            -- Set keymaps when DAP is initialized
+            dap.listeners.after.event_initialized["custom.dap.keys"] = function()
+                for _, map in ipairs(keymaps) do
+                    local key, modes, func, opts = table.unpack(map)
+                    vim.keymap.set(modes, key, func, opts)
+                end
+            end
+
+            -- Remove keymaps when DAP is terminated or disconnected
+            local reset_keys = function()
+                for _, map in ipairs(keymaps) do
+                    local key, modes, func, opts = table.unpack(map)
+                    pcall(vim.keymap.del, modes, key)
+                end
+            end
+            dap.listeners.after.event_terminated["me.dap.keys"] = reset_keys
+            dap.listeners.after.disconnected["me.dap.keys"] = reset_keys
         end,
     },
 }
