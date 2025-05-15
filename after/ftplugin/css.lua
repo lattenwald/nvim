cb6daed9d23db924c944076ecb3c7c4cc29b38e7
Alexander Q <qalexx@gmail.com>
2025-05-15 16:25:23 +0300

    css: tabstop=shiftwidth=2

--- after/ftplugin/css.lua
+++ after/ftplugin/css.lua
@@ -1,3 +1,6 @@
+vim.opt_local.tabstop = 2
+vim.opt_local.shiftwidth = 2
+
 require("config.utils").mason_install("css-lsp")
 require("config.utils").mason_install("prettierd")
 require("conform").setup({
