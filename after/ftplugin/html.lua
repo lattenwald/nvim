03b7bb0461c907a5bd4786a3ff38e80a31467c21
Alexander Q <qalexx@gmail.com>
2025-05-16 09:29:09 +0300

    html: ts=2, sw=2

--- after/ftplugin/html.lua
+++ after/ftplugin/html.lua
@@ -1,3 +1,6 @@
+vim.opt_local.tabstop = 2
+vim.opt_local.shiftwidth = 2
+
 require("config.utils").mason_install("html-lsp")
 require("config.utils").mason_install("prettierd")
 require("conform").setup({
