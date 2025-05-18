23a236d64e21c4e40ef37995a5ad825b79eacd9c
Alexander Q <qalexx@gmail.com>
2025-05-18 13:52:18 +0300

    feat: conform formatter settings; format

--- lua/plugins/50-lsp.lua
+++ lua/plugins/50-lsp.lua
@@ -10,27 +10,27 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
-        vim.api.nvim_create_autocmd("LspAttach", {
+            vim.api.nvim_create_autocmd("LspAttach", {
                 callback = function(ev)
-                local bufnr = ev.buf
-                print("LS attached buf=" .. bufnr .. " client=" .. vim.lsp.get_client_by_id(ev.data.client_id).name)
-                vim.keymap.set("n", "<leader>n", function()
-                    return ":IncRename " .. vim.fn.expand("<cword>")
-                end, { buffer = bufnr, expr = true, desc = "LSP rename symbol" })
-                vim.keymap.set("n", "]g", function()
-                    vim.diagnostic.goto_next()
-                end, { buffer = bufnr, silent = true, desc = "Go to next diagnostic" })
-                vim.keymap.set("n", "[g", function()
-                    vim.diagnostic.goto_prev()
-                end, { buffer = bufnr, silent = true, desc = "Go to previous diagnostic" })
-                vim.keymap.set("n", "<leader>d", function()
-                    vim.lsp.buf.hover()
-                end, { buffer = bufnr, desc = "LSP Hover" })
-                vim.keymap.set("n", "<leader>a", function()
-                    vim.lsp.buf.code_action()
-                end, { buffer = bufnr, desc = "LSP Code Action" })
+                    local bufnr = ev.buf
+                    print("LS attached buf=" .. bufnr .. " client=" .. vim.lsp.get_client_by_id(ev.data.client_id).name)
+                    vim.keymap.set("n", "<leader>n", function()
+                        return ":IncRename " .. vim.fn.expand("<cword>")
+                    end, { buffer = bufnr, expr = true, desc = "LSP rename symbol" })
+                    vim.keymap.set("n", "]g", function()
+                        vim.diagnostic.goto_next()
+                    end, { buffer = bufnr, silent = true, desc = "Go to next diagnostic" })
+                    vim.keymap.set("n", "[g", function()
+                        vim.diagnostic.goto_prev()
+                    end, { buffer = bufnr, silent = true, desc = "Go to previous diagnostic" })
+                    vim.keymap.set("n", "<leader>d", function()
+                        vim.lsp.buf.hover()
+                    end, { buffer = bufnr, desc = "LSP Hover" })
+                    vim.keymap.set("n", "<leader>a", function()
+                        vim.lsp.buf.code_action()
+                    end, { buffer = bufnr, desc = "LSP Code Action" })
                 end,
-        })
+            })
         end,
     },
     {
@@ -41,10 +41,33 @@ return {
     },
     {
         "stevearc/conform.nvim",
+
         config = function()
-        vim.keymap.set({ "n", "v" }, "<leader>s", function()
-            require("conform").format({ lsp_fallback = true })
-        end, { desc = "Format" })
+            require("conform").formatters.injected = {
+                ignore_errors = false,
+                lang_to_ft = {
+                    bash = "sh",
+                },
+                lang_to_ext = {
+                    bash = "sh",
+                    c_sharp = "cs",
+                    elixir = "ex",
+                    javascript = "js",
+                    julia = "jl",
+                    latex = "tex",
+                    markdown = "md",
+                    python = "py",
+                    ruby = "rb",
+                    rust = "rs",
+                    teal = "tl",
+                    typescript = "ts",
+                    jinja = "jinja",
+                    html = "html",
+                },
+            }
+            vim.keymap.set({ "n", "v" }, "<leader>s", function()
+                require("conform").format({ lsp_fallback = true })
+            end, { desc = "Format" })
         end,
     },
     {
@@ -57,10 +80,10 @@ return {
             },
         },
         config = function(_, opts)
-        local gpd = require("goto-preview")
-        gpd.setup(opts)
+            local gpd = require("goto-preview")
+            gpd.setup(opts)
 
-        vim.api.nvim_create_autocmd("LspAttach", {
+            vim.api.nvim_create_autocmd("LspAttach", {
                 group = vim.api.nvim_create_augroup("UserLspConfig", {}),
                 -- stylua: ignore
                 callback = function(ev)
@@ -71,7 +94,7 @@ return {
                     vim.keymap.set("n", "gpD", function() gpd.goto_preview_declaration() end, { desc = "Preview declaration", buffer = bufnr })
                     vim.keymap.set("n", "gpr", function() gpd.goto_preview_references() end, { desc = "Preview references", buffer = bufnr })
                 end,
-        })
+            })
         end,
     },
 }
