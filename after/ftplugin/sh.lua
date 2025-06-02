b52712ded2d27f52cc07a9f25982a1884d099f3f
Alexander Q <qalexx@gmail.com>
2025-06-02 11:07:16 +0400

    fix: formatter for ft=sh

--- after/ftplugin/sh.lua
+++ after/ftplugin/sh.lua
@@ -2,7 +2,7 @@ require("config.utils").mason_install("bash-language-server")
 require("config.utils").mason_install("beautysh")
 require("conform").setup({
     formatters_by_ft = {
-        lua = { "beautysh" },
+        sh = { "beautysh" },
     },
 })
 require("config.utils").mason_install("bash-debug-adapter")
