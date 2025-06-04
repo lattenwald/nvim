df4fa41f035f928a4dcabef860f607c7b1d62d10
Alexander Q <qalexx@gmail.com>
2025-06-04 14:01:55 +0400

    jsonc filetype

---
+++ after/ftplugin/jsonc.lua
@@ -0,0 +1,7 @@
+require("config.utils").mason_install("json-lsp")
+require("config.utils").mason_install("prettierd")
+require("conform").setup({
+    formatters_by_ft = {
+        jsonc = { "prettierd" },
+    },
+})
