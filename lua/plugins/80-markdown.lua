b8db18ac750576f2bbdc88d224207e4835f9c8ec
Alexander Q <qalexx@gmail.com>
2025-06-08 12:00:44 +0400

    render-markdown active in codecompaion buffers

--- lua/plugins/80-markdown.lua
+++ lua/plugins/80-markdown.lua
@@ -3,8 +3,8 @@ return {
         "MeanderingProgrammer/render-markdown.nvim",
         opts = {
             latex = { enabled = false },
-            file_types = { "markdown", "Avante" },
+            file_types = { "markdown", "Avante", "codecompanion" },
         },
-        ft = { "markdown", "Avante" },
+        ft = { "markdown", "Avante", "codecompanion" },
     },
 }
