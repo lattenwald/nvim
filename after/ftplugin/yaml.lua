0ddd4a0de82a7b373952780b2890496c310905c0
Alexander Q <qalexx@gmail.com>
2025-06-17 13:57:28 +0400

    yaml

--- after/ftplugin/yaml.lua
+++ after/ftplugin/yaml.lua
@@ -1,1 +1,7 @@
 require("config.utils").mason_install("yaml-language-server")
+require("config.utils").mason_install("yamlfmt")
+require("conform").setup({
+    formatters_by_ft = {
+        yaml = { "yamlfmt" },
+    },
+})
