26c4d44823a6fb5084f5e95e377b73fe840e9d8b
Alexander Q <qalexx@gmail.com>
2025-08-27 13:35:37 +0400

    feat: update Python plugin configuration

--- lua/plugins/90-python.lua
+++ lua/plugins/90-python.lua
@@ -5,4 +5,41 @@ return {
             require("dap-python").setup("uv")
         end,
     },
+    {
+        "linux-cultist/venv-selector.nvim",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "mfussenegger/nvim-dap",
+            "mfussenegger/nvim-dap-python", --optional
+        },
+        lazy = false,
+        opts = {
+            picker = {
+                type = "snacks", -- Custom type name, just to indicate we override
+                -- The function Snacks will call to pick
+                fn = function(opts, on_choice)
+                    -- opts.items is the list of venvs discovered
+                    local items = {}
+                    for _, venv in ipairs(opts.items) do
+                        table.insert(items, {
+                            text = venv.name,
+                            path = venv.path,
+                        })
+                    end
+
+                    -- Use Snacks picker
+                    require("snacks").picker({
+                        prompt = "Select Python venv",
+                        items = items,
+                        format_item = function(item)
+                            return item.text
+                        end,
+                        on_select = function(item)
+                            on_choice(item.path) -- Send selected venv path back to venv-selector
+                        end,
+                    })
+                end,
+            },
+        },
+    },
 }
