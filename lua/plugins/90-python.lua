dbbf406c9ec3ba5de250d77060bf2cf2be460fb6
Alexander Q <qalexx@gmail.com>
2025-09-16 18:37:40 +0400

    refactor: simplify venv-selector configuration to use built-in picker

--- lua/plugins/90-python.lua
+++ lua/plugins/90-python.lua
@@ -14,29 +14,14 @@ return {
         },
         lazy = false,
         opts = {
-            picker = {
-                type = "snacks", -- Custom type name, just to indicate we override
-                -- The function Snacks will call to pick
-                fn = function(opts, on_choice)
-                    -- opts.items is the list of venvs discovered
-                    local items = {}
-                    for _, venv in ipairs(opts.items) do
-                        table.insert(items, {
-                            text = venv.name,
-                            path = venv.path,
-                        })
-                    end
+            search = {},
+            options = {
+                picker = "snacks",
 
-                    -- Use Snacks picker
-                    require("snacks").picker({
-                        prompt = "Select Python venv",
-                        items = items,
-                        format_item = function(item)
-                            return item.text
-                        end,
-                        on_select = function(item)
-                            on_choice(item.path) -- Send selected venv path back to venv-selector
-                        end,
+                on_venv_activate_callback = function()
+                    local python = require("venv-selector").python()
+                    vim.notify("Activated venv: " .. python, {
+                        title = "Venv Selector",
                     })
                 end,
             },
