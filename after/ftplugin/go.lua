31d0819ad1fc14836dbe92ea02fc11cd803119ac
Alexander Q <qalexx@gmail.com>
2025-09-21 18:58:27 +0400

    feat: add Go formatting configuration to ftplugin

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,2 +1,8 @@
 require("config.utils").mason_install("gopls")
 require("config.utils").mason_install("delve")
+
+require("conform").setup({
+    formatters_by_ft = {
+        go = { "gofmt" },
+    },
+})
