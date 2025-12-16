bb26e40597457546efa3f43274152a078388cbd8
Alexander Q <qalexx@gmail.com>
2025-12-16 17:25:44 +0400

    feat: add markdown-preview.nvim with GUI and yarn conditions

--- lua/plugins/90-markdown.lua
+++ lua/plugins/90-markdown.lua
@@ -7,4 +7,14 @@ return {
         },
         ft = { "markdown", "Avante", "codecompanion" },
     },
+    {
+        "iamcco/markdown-preview.nvim",
+        cond = vim.fn.executable("yarn") == 1 and require("config.utils").has_gui(),
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        ft = { "markdown" },
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+    },
 }
