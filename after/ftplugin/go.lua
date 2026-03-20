a41b35b5b26a70302858459b87b3980771038537
Alexander Q <qalexx@gmail.com>
2026-03-20 20:46:45 +0200

    feat(go): add golangci-lint-langserver LSP configuration

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,6 +1,17 @@
 require("config.utils").mason_install("gopls")
 require("config.utils").mason_install("delve")
 
+require("config.utils").mason_install("golangci-lint-langserver")
+vim.lsp.config("golangci_lint_ls", {
+    cmd = { "golangci-lint-langserver" },
+    filetypes = { "go", "gomod" },
+    root_markers = { ".golangci.yml", ".golangci.yaml", ".golangci.toml", "go.mod" },
+    init_options = {
+        command = { "golangci-lint", "run", "--output.json.path=stdout", "--show-stats=false" },
+    },
+})
+vim.lsp.enable("golangci_lint_ls")
+
 require("conform").setup({
     formatters_by_ft = {
         go = { "gofmt" },
