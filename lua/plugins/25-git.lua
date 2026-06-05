0c20b8e1f097dc773ce59bf666f2d2377db49126
Alexander Q <qalexx@gmail.com>
2026-06-05 11:47:40 +0400

    feat(git): add <leader>hb to pick gitsigns diff base via picker

--- lua/plugins/25-git.lua
+++ lua/plugins/25-git.lua
@@ -7,6 +7,17 @@ return {
                 local gitsigns = require("gitsigns")
                 vim.keymap.set("n", "hs",         function() gitsigns.stage_hunk() end, { desc = "Stage hunk", buffer = bufnr })
                 vim.keymap.set("n", "<leader>hp", function() gitsigns.preview_hunk() end, { desc = "Preview hunk", buffer = bufnr })
+                -- Pick a branch/revision as the gitsigns diff base (global), e.g. diff against master instead of the index
+                vim.keymap.set("n", "<leader>hb", function()
+                    Snacks.picker.git_branches({
+                        confirm = function(picker, item)
+                            picker:close()
+                            if item then
+                                gitsigns.change_base(item.branch or item.commit, true)
+                            end
+                        end,
+                    })
+                end, { desc = "Gitsigns: pick diff base", buffer = bufnr })
                 vim.keymap.set("n", "<leader>hr", function() gitsigns.reset_hunk() end, { desc = "Reset hunk", buffer = bufnr })
                 vim.keymap.set("n", "<leader>hS", function() gitsigns.stage_buffer() end, { desc = "Stage buffer", buffer = bufnr })
                 vim.keymap.set("n", "<leader>hu", function() gitsigns.undo_stage_hunk() end, { desc = "Undo stage hunk", buffer = bufnr })
